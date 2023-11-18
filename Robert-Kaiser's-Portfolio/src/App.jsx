fdb81e1d452753fa381157a50cd9fd66ddce3857
Robert Kaiser <paintballmanrob@gmail.com>
2023-11-18 17:54:33 -0500

    Refactor App component structure

--- Robert-Kaiser's-Portfolio/src/App.jsx
+++ Robert-Kaiser's-Portfolio/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import { Outlet, useLocation } from 'react-router-dom'
-import { Header, Footer } from './components'
-import './App.css'
+import { Outlet } from "react-router-dom";
+import { Header, Footer } from "./components";
+import "./App.css";
 
 function App() {
   return (
-    <Header className="App-header" />
-    <main className="App-main">
-      <Outlet />
-    </main>
-    <Footer className="App-footer" />
-  )
+    <>
+      <Header className="App-header" />
+      <main className="App-main">
+        <Outlet />
+      </main>
+      <Footer className="App-footer" />
+    </>
+  );
 }
 
-export default App
+export default App;
